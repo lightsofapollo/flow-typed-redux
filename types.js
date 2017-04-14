28086b8ddf8cde6363e6dd4b5cfae40f32638f7f
James Lal <james@lightsofapollo.com>
2017-04-14 11:54:20 -0700

    Update types to remove unused junk

--- types.js
+++ types.js
@@ -1,4 +1,9 @@
-/* @flow */
+/**
+ * Global application states and actions. It is intended that every action is
+ * defined within.
+ *
+ * @flow
+ */
 import type { Component } from 'react';
 import type { Connector, ConnectedComponentClass } from 'react-redux';
 import type {
@@ -23,17 +28,3 @@ export type PublishAction = {
 export type Action = PublishAction;
 export type Store = ReduxStore<State, Action>;
 export type Dispatch = ReduxDispatch<Action>;
-
-/**
- * This type is a wrapper type for ConnectedComponentClass from redux.
- *
- * Provides easy to use type but requires that Component.props is set.
- */
-type $GetPropTypes<T> = $PropertyType<T, 'props'>
-type $GetOwnPropTypes<T> = $GetPropTypes<T> & {
-  dispatch: Dispatch
-}
-
-export type ConnectedComponent<T> =
-  $Supertype<Component<any, any, any>> &
-  ConnectedComponentClass<$GetOwnPropTypes<T>, $GetPropTypes<T>, void, void>;
